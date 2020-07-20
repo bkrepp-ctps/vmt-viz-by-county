b21f3d06a64525b3afa476450128956a76b144d8
Ben Krepp <bkrepp@ctps.org>
2020-07-20 16:42:09 -0400

    Start of JS logic for app - does not yet render properly.

---
+++ vmt_viz.js
@@ -0,0 +1,60 @@
+function popup(url) {
+	var popupWindow = window.open(url,'popUpWindow',
+		'height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=yes');
+};
+
+function vmt_visualization() {
+	// $('#about_button').hide();
+	// $('#about_button').click(function() { popup("about.html"); });
+
+	d3.json("json/counties-albers-10m.topo.json").then(function(json_data) {
+		console.log('loaded json');
+		d3.csv("csv/vmt_data.csv").then(function(csv_data) {
+			console.log('loaded csv');
+			generateMap(json_data, csv_data);
+		});
+	});
+}
+
+function generateMap(counties_topojson, vmt_data_csv) {	
+	var us = counties_topojson;
+	var vmt = vmt_data_csv;
+	
+	var counties = topojson.feature(us, us.objects.counties, (a, b) => a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0));
+	var states = topojson.feature(us, us.objects.states, (a, b) => a !== b);
+	var nation = topojson.feature(us, us.objects.nation);
+	
+	
+
+	var width = 975
+		height = 610;
+		
+	var svgContainer = d3.select("#map").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.style("border", "2px solid steelblue");
+		
+	var projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
+		
+	var geoPath = d3.geoPath()
+		.projection(projection);
+		
+	var map = svgContainer.selectAll("path")
+		.data(nation.features)
+		.enter()
+		.append("path")
+		.attr("d", function(d, i) { return geoPath(d); })
+		.style("stroke", "black")
+		.style("stroke-width", "0.25px")
+		.style("fill", "white");
+
+
+	//setSymbology();
+	// generateLegend(popChgScale, 'legend', 'Population Change (percent)');
+	
+	// $('#about_button').show();
+	
+
+	
+	
+} // generateMap
