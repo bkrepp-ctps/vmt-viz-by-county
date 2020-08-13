32ae0c7e557cd536c61c046f84126b5086011383
Ben Krepp <bkrepp@ctps.org>
2020-08-13 16:31:35 -0400

    Refactored code; moved common data and code into library.

--- vmt_viz.js
+++ vmt_viz.js
@@ -1,87 +1,22 @@
-// Value to indicate no VMT data record for {date, county} in data supplied by data provider.
-var NO_DATA = -300000000;
+// Generate animated geographic visualization of day-by-day VMT data
+// for the 48 contiguous states of the United States.
+//
+// Author: Ben Krepp
+// Date: August, 2020
 
 // Dimensions of SVG drawing area for map
 var width = 975
 	height = 610;
 var svgMapContainer;
 
-// Projection for GeoJSON data
-var projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
 // SVG geo-path generator function
 var geoPath = d3.geoPath()
-	.projection(projection);
+	.projection(vmtCommon.projection);
 
 // GeoJson counties features - populated by generateMap, referenced by symbolizeMap
 //
 var counties_features;
 
-// Threshold logarithmic scale for rendering VMT data.
-// 5-class diverging scale, inverted, with initial 'no data' step added.
-// Range values from colorbrewer2.org: https://colorbrewer2.org/#type=diverging&scheme=Spectral&n=5
-var vmt_scale = d3.scaleThreshold()
-					.domain([0, 100000, 1000000, 10000000, 100000000, Infinity])
-					.range(['gray', "#2b83ba", "#abdda4", "#ffffbf", "#fdae61", "#d7191c"]); 
-// VMT Legend labels 
-var vmt_legend_labels = ['No Data', '100,000', '1,000,000', '10,000,000', '100,000,000', '>100,000,000' ]
-
-// Threshold logarithmic scale for rendering the DELTA of VMT values beween January 2020 and a given date.
-// 9-class diverging scale, inverted, with initial 'no data' step added.
-// Range values from colorbrewer2.org: https://colorbrewer2.org/#type=diverging&scheme=Spectral&n=9
-var vmt_delta_scale = d3.scaleThreshold()
-					.domain([-200000000, 
-					         -100000000, -10000000, 1000000, -1000000,
-                       	     100000, 1000000, 10000000, 100000000, Infinity])
-					.range(['gray', 
-					        '#3288bd', '#66c2a5', '#abdda4', '#e6f598', 
-							'#ffffbf', '#fee08b', '#fdae61', '#d53e4f']);
-// Delta VMT Legend labels 
-var vmt_delta_legend_labels = ['No Data', 
-							   '>-100,000,000', '-10,000,000', '-1,000,000', '-100,000',
-                               '+100,000', '+1,000,000', '+10,000,000', '>+100,000,000'];
-
-// Dates for which we have VMT data
-var all_daytz = [   
-                '2020-01-01', '2020-01-02', '2020-01-03', '2020-01-04', '2020-01-05', '2020-01-06', '2020-01-07', 
-                '2020-01-08', '2020-01-09', '2020-01-10', '2020-01-11', '2020-01-12', '2020-01-13', '2020-01-14', 
-                '2020-01-15', '2020-01-16', '2020-01-17', '2020-01-18', '2020-01-19', '2020-01-20', '2020-01-21', 
-                '2020-01-22', '2020-01-23', '2020-01-24', '2020-01-25', '2020-01-26', '2020-01-27', '2020-01-28',
-                '2020-01-30', '2020-01-31',
-
-                '2020-02-01', '2020-02-02', '2020-02-03', '2020-02-04', '2020-02-05', '2020-02-06', '2020-02-07', 
-                '2020-02-08', '2020-02-09', '2020-02-10', '2020-02-11', '2020-02-12', '2020-02-13', '2020-02-14', 
-                '2020-02-15', '2020-02-16', '2020-02-17', '2020-02-18', '2020-02-19', '2020-02-20', '2020-02-21', 
-                '2020-02-22', '2020-02-23', '2020-02-24', '2020-02-25', '2020-02-26', '2020-02-27', '2020-02-28',
-
-                '2020-03-01', '2020-03-02', '2020-03-03', '2020-03-04', '2020-03-05', '2020-03-06', '2020-03-07', 
-                '2020-03-08', '2020-03-09', '2020-03-10', '2020-03-11', '2020-03-12', '2020-03-13', '2020-03-14', 
-                '2020-03-15', '2020-03-16', '2020-03-17', '2020-03-18', '2020-03-19', '2020-03-20', '2020-03-21', 
-                '2020-03-22', '2020-03-23', '2020-03-24', '2020-03-25', '2020-03-26', '2020-03-27', '2020-03-28',
-                '2020-03-30', '2020-03-31',
-                '2020-04-01', '2020-04-02', '2020-04-03', '2020-04-04', '2020-04-05', '2020-04-06', '2020-04-07', 
-                '2020-04-08', '2020-04-09', '2020-04-10', '2020-04-11', '2020-04-12', '2020-04-13', '2020-04-14', 
-                '2020-04-15', '2020-04-16', '2020-04-17', '2020-04-18', '2020-04-19', '2020-04-20', '2020-04-21', 
-                '2020-04-22', '2020-04-23', '2020-04-24', '2020-04-25', '2020-04-26', '2020-04-27', '2020-04-28', 
-                '2020-04-29', '2020-04-30', 
-
-                '2020-05-01', '2020-05-02', '2020-05-03', '2020-05-04', '2020-05-05', '2020-05-06', '2020-05-07', 
-                '2020-05-08', '2020-05-09', '2020-05-10', '2020-05-11', '2020-05-12', '2020-05-13', '2020-05-14', 
-                '2020-05-15', '2020-05-16', '2020-05-17', '2020-05-18', '2020-05-19', '2020-05-20', '2020-05-21', 
-                '2020-05-22', '2020-05-23', '2020-05-24', '2020-05-25', '2020-05-26', '2020-05-27', '2020-05-28', 
-                '2020-05-29', '2020-05-30', '2020-05-31', 
-
-                '2020-06-01', '2020-06-02', '2020-06-03', '2020-06-04', '2020-06-05', '2020-06-06', '2020-06-07', 
-                '2020-06-08', '2020-06-09', '2020-06-10', '2020-06-11', '2020-06-12', '2020-06-13', '2020-06-14', 
-                '2020-06-15', '2020-06-16', '2020-06-17', '2020-06-18', '2020-06-19', '2020-06-20', '2020-06-21', 
-                '2020-06-22', '2020-06-23', '2020-06-24', '2020-06-25', '2020-06-26', '2020-06-27', '2020-06-28',
-                '2020-06-29', '2020-06-30',
-                
-                '2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', 
-                '2020-07-08', '2020-07-09', '2020-07-10', '2020-07-11', '2020-07-12', '2020-07-13', '2020-07-14', 
-                '2020-07-15', '2020-07-16', '2020-07-17', '2020-07-18', '2020-07-19', '2020-07-20', '2020-07-21', 
-                '2020-07-22', '2020-07-23', '2020-07-24', '2020-07-25', '2020-07-26', '2020-07-27', '2020-07-28'
-];
-				
 // One new frame of visualization is rendered every 2000 milliseconds (2 seconds).
 var FRAME_INTERVAL = 2000;
 
@@ -118,11 +53,11 @@ function generateMap(states_geojson, counties_geojson) {
 		// .attr("transform", "translate(170,20)");
 
 	if (mode === 'absolute') {
-		my_scale = vmt_scale;
-		my_labels = vmt_legend_labels;
+		my_scale = vmtCommon.vmt_scale;
+		my_labels = vmtCommon.vmt_legend_labels;
 	} else if (mode === 'delta') {
-		my_scale = vmt_delta_scale;
-		my_labels = vmt_delta_legend_labels;
+		my_scale = vmtCommon.vmt_delta_scale;
+		my_labels = vmtCommon.vmt_delta_legend_labels;
 	}  else {
 		alert('Invalid mode: ' + mode + '. Exiting');
 		return;
@@ -172,21 +107,9 @@ function generateMap(states_geojson, counties_geojson) {
 
 } // generateMap()
 
-// Utility function to map a date string in yyyy-mm-dd format
-// into to one in conventional US-style textual format
-function make_date_text(date) {
-	var parts = date.split('-');
-	var year = parts[0]
-	var month_num = parseInt(parts[1],10);
-	var day = parseInt(parts[2],10);
-	var months = ['January', 'February', 'March', 'April', 'May', 'June',
-	              'July', 'August', 'September', 'October', 'November', 'December'];
-	var month = months[month_num-1]; // Recall 0-based array indexing
-	return month + ' ' + day + ', ' + year;
-} // make_date_text()
 
 function symbolizeMap(date_ix) {
-	var date_str = all_daytz[date_ix];
+	var date_str = vmtCommon.all_dates[date_ix];
 	var vmt_csv_fn = "csv/vmt-" + date_str + ".csv";
 	// console.log('Initiating load of ' + vmt_csv_fn);
 	d3.csv(vmt_csv_fn, function(d) {
@@ -203,7 +126,7 @@ function symbolizeMap(date_ix) {
 		};
 	}).then(function(vmt) {
 		// console.log('Rendering VMT data for ' + date_str + ' to map.')
-		var date_text = make_date_text(date_str);
+		var date_text = vmtCommon.make_date_text(date_str);
 		$('#date_field').html(date_text);
 		var vmt_recs = _.sortBy(vmt, function(rec) { return rec.fips; });
 		var i, result, temp_props, dummy_rec = {};
@@ -220,9 +143,9 @@ function symbolizeMap(date_ix) {
 				dummy_rec['state'] = temp_props['st_name'];
 				dummy_rec['county'] = temp_props['co_name'];
 				dummy_rec['date'] = date_str;
-				dummy_rec['county_vmt'] = NO_DATA;
-				dummy_rec['jan_avg_vmt'] = NO_DATA;
-				dummy_rec['delta'] = NO_DATA;
+				dummy_rec['county_vmt'] = vmtCommon.NO_DATA;
+				dummy_rec['jan_avg_vmt'] = vmtCommon.NO_DATA;
+				dummy_rec['delta'] = vmtCommon.NO_DATA;
 				vmt_recs.push(dummy_rec);
 			}
 		}
@@ -236,18 +159,18 @@ function symbolizeMap(date_ix) {
 								vmt_rec = _.find(vmt_recs, function(vmt_rec) { return vmt_rec['fips'] == d.properties['fips']; });
 								if (mode === 'absolute') {
 									data_value = vmt_rec.county_vmt;
-									retval = vmt_scale(data_value);
+									retval = vmtCommon.vmt_scale(data_value);
 								} else {
 									// Here: mode must be 'delta'
 									data_value = vmt_rec.delta;
-									retval = vmt_delta_scale(data_value);
+									retval = vmtCommon.vmt_delta_scale(data_value);
 								}
 								return retval;
 						});
 	}).then(function() {
 		var tid = setTimeout(
 					function() {
-						if (date_ix < all_daytz.length - 1) {
+						if (date_ix < vmtCommon.all_dates.length - 1) {
 							symbolizeMap(date_ix+1);
 						}
 					}, 
